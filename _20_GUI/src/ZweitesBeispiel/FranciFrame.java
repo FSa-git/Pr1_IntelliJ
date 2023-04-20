41b332fe206b74e3b3d4af284d2c2dce52fd714c
FS <francesco9721@gmail.com>
2023-04-20 18:21:33 +0200

    Todays GUI / JFrames

--- _20_GUI/src/ZweitesBeispiel/FranciFrame.java
+++ _20_GUI/src/ZweitesBeispiel/FranciFrame.java
@@ -1,4 +1,20 @@
 package ZweitesBeispiel;
 
-public class FranciFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class FranciFrame extends JFrame {
+    JLabel lb;
+
+    // Der Standard
+    public FranciFrame(String title) throws HeadlessException {
+        super(title);
+        this.setSize(300,300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.setLayout(new FlowLayout()); // Layout-Manager
+
+        lb = new JLabel("Hallo ADV World!");
+        this.add(lb);
+    }
 }
--- _20_GUI/src/ZweitesBeispiel/Tester.java
+++ _20_GUI/src/ZweitesBeispiel/Tester.java
@@ -1,4 +1,13 @@
 package ZweitesBeispiel;
 
 public class Tester {
+
+    public static void main(String[] args) {
+
+        FranciFrame fr = new FranciFrame("Mein Fenster im Flowlayout");
+
+        fr.setVisible(true);
+
+    }
+
 }
