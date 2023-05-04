aa12c876c855a70d239b18505a9ff713f631d940
FS <francesco9721@gmail.com>
2023-05-04 10:44:44 +0200

    JButton_Zahlenschloss_Prg. 1

---
+++ _22_GUI Button/src/Safe/SafeTester.java
@@ -0,0 +1,13 @@
+package Safe;
+
+public class SafeTester {
+    public static void main(String[] args) {
+
+        Zahlenschloss frm = new Zahlenschloss("ADV Zahlenschloss");
+
+        frm.setBounds(600,400,600, 200);
+        frm.setVisible(true);
+
+    }
+
+}
---
+++ _22_GUI Button/src/Safe/Zahlenschloss.java
@@ -0,0 +1,52 @@
+package Safe;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Zahlenschloss extends JFrame implements ActionListener {
+
+    // Deklaration
+    JButton bEins, bZwei;
+
+    // -- DAS BRAUCHT MAN IMMER --
+    public Zahlenschloss(String title) {
+        super(title);
+
+        // Buttons festlegen
+        JButton bEins = new JButton("1");
+        JButton bZwei = new JButton("2");
+
+        // Den Jframe einsetzen
+        bEins.addActionListener(this);
+        bZwei.addActionListener(this);
+
+        // Layout festsetzen
+        this.setLayout(new FlowLayout());
+
+
+
+        this.add(bEins);
+        this.add(bZwei);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    // -- BIS HIER --
+
+    @Override
+    public void actionPerformed(ActionEvent evt) { // evt -> Event, kann aber auch anders heißen
+
+        if (evt.getActionCommand().equals("1")){
+
+            // Die Fläche und den Hintergrund auf Grün setzen
+            this.getContentPane().setBackground(Color.green);
+        }
+
+        if (evt.getActionCommand().equals("2")){
+
+            // Die Fläche und den Hintergrund auf Blau setzen
+            this.getContentPane().setBackground(Color.blue);
+        }
+
+    }
+}
