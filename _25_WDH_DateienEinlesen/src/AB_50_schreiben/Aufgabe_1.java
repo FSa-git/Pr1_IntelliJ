27838f8f360c68fe5707fb93b2abce7a7e2f4f84
FS <francesco9721@gmail.com>
2023-05-26 12:48:11 +0200

    Dateien Einlesen Aufgabe_1 WE_Prg. 1

---
+++ _25_WDH_DateienEinlesen/src/AB_50_schreiben/Aufgabe_1.java
@@ -0,0 +1,51 @@
+package AB_50_schreiben;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class Aufgabe_1 {
+
+    static String myPath = "/Users/francescosakautzki/Desktop/HeutePrg/Pr1_IntelliJ/_25_WDH_DateienEinlesen/src/AB_50_schreiben/zahlen.txt";
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter writer = null;
+        String trennzeichen = "\t";
+
+        Charset charset = Charset.forName("UTF-8");
+
+        try{
+            writer = Files.newBufferedWriter(Path.of(myPath), charset, StandardOpenOption.WRITE);
+
+            writer.write("Geschwindigkeit [km/h]" + trennzeichen + "ca. Anhalteweg [m]");
+            writer.newLine();
+            writer.write("____________________________");
+            writer.newLine();
+
+            int geschwindigkeit = 0, anhalteweg = 0;
+
+            for (int i = 0; i <= 100; i = i + 10){
+                geschwindigkeit = i;
+                anhalteweg = i;
+
+                writer.write(geschwindigkeit + trennzeichen + anhalteweg);
+                writer.newLine();
+            }
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            if (writer != null){
+                writer.close();
+            }
+        }
+
+    }
+
+}
