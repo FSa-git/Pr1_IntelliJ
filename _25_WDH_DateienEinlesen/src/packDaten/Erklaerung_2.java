7a5e660b7eb6fd57cdf6bf8499fb4b7e0e8b1275
FS <francesco9721@gmail.com>
2023-05-26 11:57:28 +0200

    Dateien Einlesen Erklaerung mit WE_Prg. 1

--- _25_WDH_DateienEinlesen/src/packDaten/Erklaerung_2.java
+++ _25_WDH_DateienEinlesen/src/packDaten/Erklaerung_2.java
@@ -4,20 +4,44 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
 public class Erklaerung_2 {
 
-    static String myPath = "C:\\tmp\\zahlen.txt";
+    static String myPath = "/Users/francescosakautzki/Desktop/HeutePrg/Pr1_IntelliJ/_25_WDH_DateienEinlesen/src/packDaten/daten3_erklaerung.txt";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter writer = null;
+        String trennzeichen = "\t";
 
         Charset charset = Charset.forName("UTF-8"); // kann auch ohne gehen..
-        try (BufferedWriter writer = Files.newBufferedWriter(myPath, charset, StandardOpenOption)){ // Optionen gehen by "try" auch
+        try{
+            writer = Files.newBufferedWriter(Path.of(myPath), charset, StandardOpenOption.WRITE);
 
+            writer.write("zahl" + trennzeichen + "quadratzahl" + trennzeichen + "wurzel");
+            writer.newLine();
+            writer.write("************");
+            writer.newLine();
+            int zahl = 0, quadratzahl = 0;
+            double wurzel = 0.0;
+
+            for (int i = 0; i <= 10; i++){
+                zahl = i;
+                quadratzahl = (int) Math.pow(quadratzahl, 2);
+                wurzel = Math.sqrt(zahl);
+
+                writer.write(zahl + trennzeichen + quadratzahl + trennzeichen + wurzel);
+                writer.newLine();
+            }
         }
         catch(IOException e){
-
+            e.printStackTrace();
+        }
+        finally{
+            if (writer != null){
+                writer.close();
+            }
         }
     }
 }
