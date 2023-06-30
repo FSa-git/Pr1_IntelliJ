49b019bf6c7d8d53fe1dd89af3e77741f26641be
FS <francesco9721@gmail.com>
2023-06-30 16:17:29 +0200

    Java Data Base Connection(DB_Projekt)_Zugriff auf DB KrustyKrab/Drinks_PR1

--- _27_JDBC/src/_32_FastFoodBestellTool/FoodDB_GUI.java
+++ _27_JDBC/src/_32_FastFoodBestellTool/FoodDB_GUI.java
@@ -27,30 +27,13 @@ public class FoodDB_GUI extends JDialog{
             }
         });
 
-        /*
-        // Optionen für comboBox1 hinzufügen
-        comboBox1.addItem("Double Cheese Burger");
-        comboBox1.addItem("Ceaser Burger");
-        comboBox1.addItem("Belgium Fries");
 
-        // Optionen für comboBox2 hinzufügen
-        comboBox2.addItem("Option A");
-        comboBox2.addItem("Option B");
-        comboBox2.addItem("Option C");
-
-        // Optionen für comboBox3 hinzufügen
-        comboBox3.addItem("Choice X");
-        comboBox3.addItem("Choice Y");
-        comboBox3.addItem("Choice Z");
-
-        // Optionen für comboBox4 hinzufügen
-        comboBox4.addItem("Item 1");
-        comboBox4.addItem("Item 2");
-        comboBox4.addItem("Item 3");
-*/
         loadComboBoxOptionsFromDatabase("comboBox1", comboBox1);
         loadComboBoxOptionsFromDatabase("comboBox2", comboBox2);
         loadComboBoxOptionsFromDatabase("comboBox3", comboBox3);
+
+        loadDrinksDatabase("comboBox4", comboBox4);
+
 
         // loadComboBoxOptionsFromDatabase(comboBox1);
 
@@ -67,6 +50,38 @@ public class FoodDB_GUI extends JDialog{
 
             // SQL-Abfrage ausführen
             String query = "SELECT Bezeichnung FROM Foods ORDER BY CASE WHEN Bezeichnung = 'Bitte Auswählen' THEN 0 ELSE 1 END, Preis ASC"; // Database column (2)
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            // JComboBox-Optionen aktualisieren
+            while (resultSet.next()) {
+                String option = resultSet.getString("Bezeichnung");
+                comboBox.addItem(option);
+            }
+
+            // Ressourcen freigeben
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Fehler beim Abrufen der Daten aus der Datenbank", "Fehler", JOptionPane.ERROR_MESSAGE);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Drinks-Tabelle
+    private void loadDrinksDatabase(String columnName, JComboBox comboBox) {
+        try {
+
+            Class.forName("org.mariadb.jdbc.Driver");
+
+            // MySQL-Datenbankverbindung herstellen
+            Connection connection = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:8889/KrustyKrab", "root", "root");
+
+            // SQL-Abfrage ausführen
+            String query = "SELECT Bezeichnung FROM Drinks ORDER BY CASE WHEN Bezeichnung = 'Bitte Auswählen' THEN 0 ELSE 1 END, Preis ASC"; // Database column (2)
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
 
